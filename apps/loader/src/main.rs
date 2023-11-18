5dc75a3b31a8b205c0e6e9be93c13ead5abe8822
xiuzz <1519072696@qq.com>
2023-11-19 01:40:39 +0800

    lab1

--- apps/loader/src/main.rs
+++ apps/loader/src/main.rs
@@ -1,87 +1,23 @@
 #![cfg_attr(feature = "axstd", no_std)]
 #![cfg_attr(feature = "axstd", no_main)]
-
 #[cfg(feature = "axstd")]
+
 use axstd::println;
-
 const PLASH_START: usize = 0x22000000;
-
-struct ImageHeader{
-    ptr_len: usize
-}
-
-struct AppHeader {
-    start: usize,
-    size: usize,
-    content: &'static [u8],
-}
-
-impl AppHeader {
-    pub fn new(start: usize, size: usize, content: &'static [u8]) -> Self {
-        Self {
-            start,
-            size,
-            content,
-        }
-    }
-}
-
-impl ImageHeader {
-
-    pub fn new(ptr_len: usize) -> Self {
-        Self {
-            ptr_len
-        }
-    }
-
-    #[inline]
-    pub fn load_app_nums(&self, image_start:usize) -> usize {
-        let app_size = self.read_bytes(image_start, self.ptr_len);
-        self.bytes_to_usize(app_size)
-    }
-
-    #[inline]
-    pub fn load_app(&self, mut app_start :usize) -> AppHeader{
-        let tmp = self.read_bytes(app_start, self.ptr_len);
-        let app_size = self.bytes_to_usize(tmp);
-        app_start += self.ptr_len;
-        AppHeader::new(app_start, app_size, self.read_bytes(app_start, app_size))
-    }
-
-    #[inline]
-    fn read_bytes(&self, ptr: usize, ptr_len: usize) -> &'static [u8] {
-        unsafe { core::slice::from_raw_parts(ptr as *const u8, ptr_len) }
-    }
-
-    #[inline]
-    fn bytes_to_usize(&self, binary: &[u8]) -> usize {  
-        let high_byte = binary[0] as usize;
-        let low_byte = binary[1] as usize;
-        (high_byte << 8) | low_byte
-    }
-}
-
-
 #[cfg_attr(feature = "axstd", no_mangle)]
+
 fn main() {
-    //image header
-    let ptr_len = 2;
-    let image_header  = ImageHeader::new(ptr_len);
-    
-    let app_nums: usize= image_header.load_app_nums(PLASH_START);
+    let apps_start = PLASH_START as *const u8;
+    let apps_size = 32; // Dangerous!!! We need to get accurate size of apps.
+    println!("Load payload ...");
+    let code = unsafe { core::slice::from_raw_parts(apps_start, apps_size)
+    };
+    println!("content: {:#x}", bytes_to_usize(&code[..8]));
+    println!("Load payload ok!");
+}
 
-    println!("the app nums : {}", app_nums);
-
-    //app x
-
-    let mut app_start = PLASH_START + ptr_len;
-    for _ in 0..app_nums {
-        let app_header = image_header.load_app(app_start);
-        println!("start:{:#}",app_header.start);
-        println!("size:{}", app_header.size);
-        println!("context:{:?}",app_header.content);
-        println!("......................................");
-        app_start += app_header.size + ptr_len;
-    }
+#[inline]
+fn bytes_to_usize(bytes: &[u8]) -> usize {
+    usize::from_be_bytes(bytes.try_into().unwrap())
 }
 
