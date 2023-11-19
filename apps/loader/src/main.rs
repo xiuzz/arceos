ec92f2931c7a9fda981e2151b46209c4713851d7
xiuzz <1519072696@qq.com>
2023-11-19 15:23:27 +0800

    lab3

--- apps/loader/src/main.rs
+++ apps/loader/src/main.rs
@@ -4,9 +4,25 @@
 
 #[cfg(feature = "axstd")]
 use axstd::println;
-const PLASH_START: usize = 0x22000000;
-#[cfg_attr(feature = "axstd", no_mangle)]
 
+const PLASH_START: usize = 0x22000000;
+const SYS_HELLO: usize = 1;
+const SYS_PUTCHAR: usize = 2;
+static mut ABI_TABLE: [usize; 16] = [0; 16];
+
+
+fn register_abi(num: usize, handle: usize) {
+    unsafe { ABI_TABLE[num] = handle; }
+}
+fn abi_hello() {
+    println!("[ABI:Hello] Hello, Apps!");
+}
+
+fn abi_putchar(c: char) {
+    println!("[ABI:Print] {c}");
+}
+
+#[cfg_attr(feature = "axstd", no_mangle)]
 fn main() {
     let load_start = PLASH_START as *const u8;
     let load_size = 32; // Dangerous!!! We need to get accurate size of apps.
@@ -29,13 +45,29 @@ fn main() {
     run_code.as_ptr());
     println!("Load payload ok!");
 
+    register_abi(SYS_HELLO, abi_hello as usize);
+    register_abi(SYS_PUTCHAR, abi_putchar as usize);
+
     println!("Execute app ...");
-    // execute app]
+    
+    let arg0:u8 = b'A';
+
+    // execute app
     unsafe { core::arch::asm!("
+        li      t0, {abi_num}
+        slli    t0, t0, 3
+        la      t1, {abi_table}
+        add     t1, t1, t0
+        ld      t1, (t1)
+        jalr    t1
         li      t2, {run_start}
         jalr    t2
         j       .",
         run_start = const RUN_START,
+        abi_table = sym ABI_TABLE,
+        //abi_num = const SYS_HELLO,
+        abi_num = const SYS_PUTCHAR,
+        in("a0") arg0,
     )}
 
 }
