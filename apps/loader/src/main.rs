a0c89a9514362d9d8a05798c5a19116d6bfee73c
xiuzz <1519072696@qq.com>
2023-11-20 10:24:22 +0800

    lab5

--- apps/loader/src/main.rs
+++ apps/loader/src/main.rs
@@ -33,10 +33,13 @@ fn main() {
     println!("load code {:?}; address [{:?}]", load_code,
     load_code.as_ptr());
 
+    // switch aspace from kernel to app
+    unsafe { init_app_page_table(); }
+    unsafe { switch_app_aspace(); }
     // app running aspace
     // SBI(0x80000000) -> App <- Kernel(0x80200000)
     // 0xffff_ffc0_0000_0000
-    const RUN_START: usize = 0xffff_ffc0_8010_0000;
+    const RUN_START: usize = 0x4010_0000;
     let run_code = unsafe {
         core::slice::from_raw_parts_mut(RUN_START as *mut u8, load_size)
     };
@@ -69,3 +72,26 @@ fn bytes_to_usize(bytes: &[u8]) -> usize {
     usize::from_be_bytes(bytes.try_into().unwrap())
 }
 
+//
+// App aspace
+//
+#[link_section = ".data.app_page_table"]
+static mut APP_PT_SV39: [u64; 512] = [0; 512];
+unsafe fn init_app_page_table() {
+    // 0x8000_0000..0xc000_0000, VRWX_GAD, 1G block
+    APP_PT_SV39[2] = (0x80000 << 10) | 0xef;
+    // 0xffff_ffc0_8000_0000..0xffff_ffc0_c000_0000, VRWX_GAD, 1G block
+    APP_PT_SV39[0x102] = (0x80000 << 10) | 0xef;
+    // 0x0000_0000..0x4000_0000, VRWX_GAD, 1G block
+    APP_PT_SV39[0] = (0x00000 << 10) | 0xef;
+    // For App aspace!
+    // 0x4000_0000..0x8000_0000, VRWX_GAD, 1G block
+    APP_PT_SV39[1] = (0x80000 << 10) | 0xef;
+}
+unsafe fn switch_app_aspace() {
+    use riscv::register::satp;
+    let page_table_root = APP_PT_SV39.as_ptr() as usize -
+    axconfig::PHYS_VIRT_OFFSET;
+    satp::set(satp::Mode::Sv39, 0, page_table_root >> 12);
+    riscv::asm::sfence_vma_all();
+}
